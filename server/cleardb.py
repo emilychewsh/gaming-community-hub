9d29fa1ef0d53bcc69dc605d07492a67aa06122a
Emily Chew <emilychewsh@gmail.com>
2024-08-03 18:00:36 +1000

    clear all db data

---
+++ server/cleardb.py
@@ -0,0 +1,15 @@
+from config import app, db
+from models import User, Game, Review, ReviewLike, Favourite
+
+def clear_all_records():
+    db.session.query(User).delete()
+    db.session.query(Game).delete()
+    db.session.query(Review).delete()
+    db.session.query(ReviewLike).delete()
+    db.session.query(Favourite).delete()
+    db.session.commit()
+    print("All records have been cleared!")
+
+# if __name__ == "__main__":
+#     with app.app_context():
+#         clear_all_records()
