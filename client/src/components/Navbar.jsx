623b50c02c3a7ba3125ffdb8d275f30b7744445e
Emily Chew <emilychewsh@gmail.com>
2024-08-14 16:25:25 +1000

    Create Navbar and homepage

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">Gaming Hub</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <NavDropdown title="Games" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="/games">All Games</NavDropdown.Item>
+              <NavDropdown.Item href="/games/rpg">RPG</NavDropdown.Item>
+              <NavDropdown.Item href="/games/action">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/games/racing">Racing</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/signup">Sign Up</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect, useState } from 'react'
+import "./homePage.css"
+
+
+function HomePage() {
+
+
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+export default HomePage
